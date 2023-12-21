dc1e46441d5bb2549e7f21309d957488c31bf3b9
ewhuet <ewhuet@gmail.com>
2023-12-21 22:23:20 +0200

    created a factory function to create and hold the gameboard, included for loops to develop the board and will only return the board

---
+++ app.js
@@ -0,0 +1,18 @@
+function createGameboard () {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    const getBoard = () => board;
+
+    return {getBoard};
+
+}
+
